1cebaf3aa56e8fcf0b9cc13fc622fdda5069bfce
root <root@ip-172-31-42-14.us-west-2.compute.internal>
2014-08-31 09:53:25 +0000

    First Commit

---
+++ app/views/hello.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Chandan Goopta</title>
+	<style>
+		@import url(//fonts.googleapis.com/css?family=Lato:700);
+
+		body {
+			margin:0;
+			font-family:'Lato', sans-serif;
+			text-align:center;
+			color: #999;
+		}
+
+		.welcome {
+			width: 300px;
+			height: 200px;
+			position: absolute;
+			left: 50%;
+			top: 50%;
+			margin-left: -150px;
+			margin-top: -100px;
+		}
+
+		a, a:visited {
+			text-decoration:none;
+		}
+
+		h1 {
+			font-size: 32px;
+			margin: 16px 0 0 0;
+		}
+	</style>
+</head>
+<body>
+	<div class="welcome">
+		<a href="http://twitter.com/chandangoopta" title="Chandan Goopta on Twitter"><img src="https://fbcdn-sphotos-h-a.akamaihd.net/hphotos-ak-xfa1/v/t1.0-9/487621_152580514903544_588224632_n.jpg?oh=6ec36e329d9c72212c56116b2c3d33fa&oe=54696CE6&__gda__=1417614680_9404d75a5ff98559eb4170ce16dc9420" height="300" width="217" alt="Chandan Goopta"></a>
+		<h1>Under Construction :P </h1>
+	</div>
+</body>
+</html>
